61ce62a9596f497c2315a948f3246794cc7031e2
sufu <fusu.robert@gmail.com>
2017-01-04 22:03:21 -0800

    added http requests of weather data

--- emailer.py
+++ emailer.py
@@ -1,3 +1,5 @@
+import requests
+
 def get_emails():
     emails = {}
 
@@ -21,10 +23,33 @@ def get_schedule():
 
     return schedule
 
+def get_weather_forcast():
+    url = 'http://api.openweathermap.org/data/2.5/weather?q=SanJose&units=metric&appid=oops'
+    weather_request = requests.get(url)
+    weather_json = weather_request.json()
+    print(weather_json)
+
+    description = weather_json['weather'][0]['description']
+    print(description)
+
+    temp_min = weather_json['main']['temp_min']
+    temp_max = weather_json['main']['temp_max']
+
+    print(temp_min)
+    print(temp_max)
+
+    forecast = 'The foreast for today is '
+    forecast += description + ' with a high of ' + str(temp_max)
+    forecast += ' and a low of ' + str(temp_min) + '.'
+    print(forecast)
+
 def main():
     emails = get_emails()
     print(emails)
     schedule = get_schedule()
     print(schedule)
 
+    get_weather_forcast()
+
 main()
+
